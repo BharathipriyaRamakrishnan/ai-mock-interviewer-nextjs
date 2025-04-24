5a440adc8d5d748d3c0a937fda4bc4dd20115176
BharathipriyaRamakrishnan <bharathipriyaramakrishnan@gmail.com>
2025-04-24 22:30:49 +0530

    " Static UI Card"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -21,14 +23,19 @@ const page = () => {
       <section className='flex flex-col gap-6 mt-8'>
         <h2>Your Interview</h2>
         <div className='interviews-section'>
-          <p>You haven't started an interview yet</p>
+         {dummyInterviews.map((interview) => (
+           <InterviewCard {...interview} key = {interview.id} />
+         ))}
         </div>
       </section>
 
       <section className='flex flex-col gap-6 mt-8'>
         <h2>Take an Interview</h2>
         <div className='interviews-section'>
-          <p>There are no interviews available</p>
+        {dummyInterviews.map((interview) => (
+           <InterviewCard {...interview} key = {interview.id} />
+         ))}
+         {/* <p>You haven't taken any interviews</p> */}
         </div>
       </section>
     </>
