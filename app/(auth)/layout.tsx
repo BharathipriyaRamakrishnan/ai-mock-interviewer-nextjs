2ae7b2c5ec706a921ff2b1deb6826a832502b8cd
BharathipriyaRamakrishnan <bharathipriyaramakrishnan@gmail.com>
2025-04-28 22:24:26 +0530

    "Implememt Interview Session"

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,7 @@
 import { isAuthenticated } from '@/lib/actions/auth.action';
+import { redirect } from 'next/navigation';
 import { ReactNode } from 'react'
+
 
 const AuthLayout = async ({children}:{children: ReactNode  }) => {
     const isUserAuthenticated = await isAuthenticated();
