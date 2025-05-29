3d9c10809c293536390cc53408c52233a2ba1c70
BharathipriyaRamakrishnan <bharathipriyaramakrishnan@gmail.com>
2025-05-29 10:06:43 +0530

    Rearrange

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -21,7 +21,13 @@ interface SavedMessage {
     content: string;
 }
 
-const Agent = ({userName, userId, type, interviewId, questions}: AgentProps) => {
+const Agent = ({
+    userName, 
+    userId, 
+    type, 
+    interviewId, 
+    questions}: 
+    AgentProps) => {
     const router = useRouter();
     const [isSpeaking, setIsSpeaking] = useState(false);
     const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
